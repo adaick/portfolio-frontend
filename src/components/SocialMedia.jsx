27219d4be17e25629108b1d411c662499011df77
Adarsh Mallick <adarshmallick@gmail.com>
2025-11-24 01:18:55 +0100

    Add aria-labels to social media links for accessibility
    Added aria-label attributes to LinkedIn and Instagram anchor tags to improve accessibility. Also updated the import for BsInstagram to use the correct icon library.

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+// import { BsTwitter, BsInstagram } from 'react-icons/bs';
 // import { FaFacebookF } from 'react-icons/fa';
-import { CiLinkedin } from "react-icons/ci";
+import { CiLinkedin, BsInstagram } from "react-icons/ci";
 
 const SocialMedia = () => (
   <div className="app__social">
@@ -9,11 +9,11 @@ const SocialMedia = () => (
       <BsTwitter />
     </div> */}
     <div>
-      <a href="https://www.linkedin.com/in/adarshmallick/" target="_blank" rel="noopener noreferrer"></a>
+      <a href="https://www.linkedin.com/in/adarshmallick/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"></a>
       <CiLinkedin />
     </div>
     <div>
-      <a href="https://www.instagram.com/i.adaick/" target="_blank" rel="noopener noreferrer"></a>
+      <a href="https://www.instagram.com/i.adaick/" target="_blank" rel="noopener noreferrer" aria-label="Instagram"></a>
       <BsInstagram />
     </div>
   </div>
